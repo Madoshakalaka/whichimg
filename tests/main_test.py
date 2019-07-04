fa2f830e3a0fb1e0fbbab1c2dd69dc4527113d74
matt <syan4@ualberta.ca>
2019-07-04 15:57:37 -0600

    developing

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,10 +1,17 @@
+import glob
 import os
 import subprocess
 import sys
 import unittest
 from os import path
+from cv2 import cv2
 
 from whichimg import main
+from whichimg.main import ImageTeller
+
+
+def get_fixture_img(name):
+    return cv2.imread(path.join('fixtures', name + '.png'))
 
 
 class MyTestCase(unittest.TestCase):
@@ -14,22 +21,53 @@ class MyTestCase(unittest.TestCase):
         MyTestCase.oldDir = os.getcwd()
         os.chdir(os.path.dirname(__file__))
 
-    def test_stuff(self):
-        pass
+    def test_shapeToIndex(self):
+        # 10: phages 15: emerald 20: bloody_sea
+        a = get_fixture_img('phage')
+        b = get_fixture_img('phage_blue_face')
+        c = get_fixture_img('emerald')
+        d = get_fixture_img('bloody_sea')
+        e = get_fixture_img('phage_bw')
 
+        t = ImageTeller([a, b, c, d, e])
+        answer = {(10, 10): [0, 1, 4], (15, 15): [2], (20, 20): [3]}
+        index_dict = t._shapeToImgIndexes
+        for shape, indexes in index_dict.items():
+            self.assertIn(shape, answer)
+            self.assertEqual(indexes, answer[shape])
+
+
+
+    def test_stuff(self):
+        a = get_fixture_img('phage')
+
+        b = get_fixture_img('phage_blue_face')
+
+        absdiff = cv2.absdiff(b, a)
+        print(absdiff)
+        cv2.imshow('lmao', absdiff)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
     def test_cmd(self):
-        cmd = ' '.join((sys.executable, path.join('..','whichimg','main.py')))
+        cmd = ' '.join((sys.executable, path.join('..', 'whichimg', 'main.py')))
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, _ = p.communicate()
 
         res = stdout.decode(encoding='utf-8').strip()
         print("command line result:", res)
 
-
     def tearDown(self) -> None:
-
         os.chdir(MyTestCase.oldDir)
+
+
+def generate_grey_scale_fixtures():
+    files = [file for file in glob.glob(path.join('fixtures', '*.png')) if not (file.endswith('bw.png'))]
+    images = [cv2.imread(file, cv2.IMREAD_GRAYSCALE) for file in files]
+    print(len(images), 'non bw images is going to be converted to greyscale')
+    for i in range(len(images)):
+        cv2.imwrite(files[i].split('.')[0] + '_bw.png', images[i])
+
 
 if __name__ == '__main__':
     unittest.main()
--- whichimg/main.py
+++ whichimg/main.py
@@ -1,5 +1,71 @@
 #!/usr/bin/env python3
 import argparse
+from typing import List, Dict, Tuple
+
+import numpy as np
+from cv2 import cv2
+
+
+class Procedure:
+    def __init__(self, r_c: Tuple[int, int], bgr_color: Tuple[int, int, int], limited_indexes: List[int]):
+        """
+        if the pixel at row, column <r_c> has color <bgrColor>, then we can shrink our candidates to <limited_indexes>
+
+        :param r_c:
+        :param bgr_color:
+        :param limited_indexes:
+        """
+        self.r_c = r_c
+        self.bgr_color = bgr_color
+        self.limited_indexes = limited_indexes
+
+
+class ImageTeller:
+    def __init__(self, possible_images: List[np.ndarray]):
+        """
+        An ImageTeller analyzes a list of given images upon creation to know their differences.
+        It takes time to analyze. Please only initialize once.
+        :param possible_images: a list of numpy images
+        """
+        self._possible_images = possible_images
+
+        self._shapeToImgIndexes: Dict[Tuple[int, int], List[int]] = dict()
+
+        for index, img in enumerate(possible_images):
+            if img.shape[2] != 3:
+                img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+            shape_tuple = tuple(img.shape[:2])
+
+            if shape_tuple in self._shapeToImgIndexes:
+                self._shapeToImgIndexes[shape_tuple].append(index)
+            else:
+                self._shapeToImgIndexes[shape_tuple] = [index]
+
+        self._shapeToProcedures: Dict[Tuple[int, int], List[Procedure]] = {shape: [] for shape in
+                                                                           self._shapeToImgIndexes.keys()}
+
+        for shape, imgIndexes in self._shapeToImgIndexes.items():
+            self._shapeToProcedures[shape] = self._produce_procedures_of_shape(shape)
+
+    def _produce_procedures_of_shape(self, shape):
+        procedures = []
+        indexes = self._shapeToImgIndexes[shape]
+        images = [self._possible_images[i] for i in indexes]
+
+        progress = 0
+        while progress < len(indexes) - 1:
+            thisIndex = indexes[progress]
+            nextIndex = indexes[progress + 1]
+            thisPic = images[progress]
+            nextPic = images[progress + 1]
+
+            absolute_diff = cv2.absdiff(thisPic, nextPic)
+            mask = np.any(absolute_diff != [0,0,0], axis=-1)
+            nonzero_r_c = np.where(mask)
+            assert np.any(nonzero_r_c), "Got identical images"
+
+
 
 def cmd():
     parser = argparse.ArgumentParser(
@@ -18,7 +84,6 @@ def cmd():
     # add your command line program here
 
     print("reserved command line entry for python package whichimg")
-
 
 
 def main():
